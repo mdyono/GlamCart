931b169582eb707c9531aa6e180141898ffe62c8
mdyono <mohammedyounus45249@gmail.com>
2025-02-26 16:59:22 +0530

    product display done

--- GlamCart/src/App.jsx
+++ GlamCart/src/App.jsx
@@ -3,27 +3,29 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Shop from './Pages/Shop'
-import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import Cart from './Pages/Cart'
-import LoginSignup from './Pages/loginSignup'
+import ShopCategory from './Pages/ShopCategory'
+import men_banner from './Components/assets/banner_mens.png'
+import women_banner from './Components/assets/banner_women.png'
+import kid_banner from './Components/assets/banner_kids.png'
+
+import LoginSignup from './Pages/LoginSignup'
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Shop/>}>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-        <Route path='/product' element={<Product/>}/>
-        <Route path=':productId' element={<Product/>}/>
-        </Route>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<LoginSignup/>}/>
-      </Routes>
-      </BrowserRouter>
+  <Navbar />
+  <Routes>
+    <Route path='/' element={<Shop />} />
+    <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+    <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+    <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
+    <Route path='/product/:productId' element={<Product />} />
+    <Route path='/cart' element={<Cart />} />
+    <Route path='/login' element={<LoginSignup />} />
+  </Routes>
+</BrowserRouter>
       
     </div>
   )
--- GlamCart/src/Components/items/Items.jsx
+++ GlamCart/src/Components/items/Items.jsx
@@ -2,15 +2,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "./item.css";
+import { Link } from 'react-router-dom';
 
-function Items({ image, name, new_price, old_price }) {
+function Items(props) {
   return (
     <div className='item'>
-      <img src={image} alt={name} />
-      <p>{name}</p>
+      <Link to={`/product/${props.id}`}><img src={props.image} alt='' /></Link>
+      <p>{props.name}</p>
       <div className='item-prices'>
-        <div className='item-price-new'>{new_price}</div>
-        <div className='item-price-old'>{old_price}</div>
+        <div className='item-price-new'>{props.new_price}</div>
+        <div className='item-price-old'>{props.old_price}</div>
       </div>
     </div>
   );
